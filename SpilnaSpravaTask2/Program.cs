4b5d234ff7dbd98fec6e730175121f490ae12e66
blek213 <blek213@gmail.com>
2018-05-17 18:16:54 +0300

    Create app, addding NTextCat and simple code for string

---
+++ SpilnaSpravaTask2/Program.cs
@@ -0,0 +1,48 @@
+﻿using NTextCat;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpilnaSpravaTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Указываем путь папки с файлами
+            string setting = @"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\Files\";
+
+            string[] files = Directory.GetFiles(setting);
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void IdentityLanguage()
+        {
+            var factory = new RankedLanguageIdentifierFactory();
+            var identifier = factory.Load(@"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\LanguageModels\Core14.profile.xml");
+
+            var languages = identifier.Identify("Larin es el mejor");
+
+            var mostCertainLanguage = languages.FirstOrDefault();
+            if (mostCertainLanguage != null)
+            {
+                string LarMan = mostCertainLanguage.Item1.Iso639_3;
+
+                Console.WriteLine(LarMan);
+            }
+            else
+            {
+                Console.WriteLine("The language couldn’t be identified with an acceptable degree of certainty");
+
+            }
+        }
+    }
+}

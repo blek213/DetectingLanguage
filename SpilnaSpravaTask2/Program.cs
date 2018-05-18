66617f656dd59129c5a243d9cdcf3a5b0b62d40d
blek213 <blek213@gmail.com>
2018-05-18 12:56:11 +0300

    ваш заголовок

--- SpilnaSpravaTask2/Program.cs
+++ SpilnaSpravaTask2/Program.cs
@@ -10,39 +10,82 @@ namespace SpilnaSpravaTask2
 {
     class Program
     {
+        //Указываем путь папки с файлами
+        public static string setting = @"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\Files\";
+
+        //Указываем путь для Core14.profile.xml
+        public static string identifierFactory = @"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\LanguageModels\Core14.profile.xml";
+
         static void Main(string[] args)
         {
-            //Указываем путь папки с файлами
-            string setting = @"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\Files\";
 
             string[] files = Directory.GetFiles(setting);
+
             foreach (string s in files)
             {
-                Console.WriteLine(s);
+
+                //   Console.WriteLine(lines);
             }
+
+            string lines = System.IO.File.ReadAllText(@"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\Files\OnlyRussian.txt");
+
+            IdentitySentense(lines);
+
 
             Console.ReadKey();
         }
 
-        public static void IdentityLanguage()
+        public static void IdentitySentense(string lines)
+        {
+            int StartPoint = 0;
+            int EndPoint = 0;
+            int val = 0;
+
+            foreach (var b in lines)
+            {
+                val++;
+                if (b == '!' || b == '.' || b == ',' || b == '?')
+                {
+                    EndPoint = val;
+
+                    string sentense = "";
+
+                    for (int i = StartPoint; i < EndPoint; i++)
+                    {
+                        sentense += lines[i];
+
+                    }
+
+                    string language = IdentityLanguage(sentense);
+
+                    if (language == "System error: The language couldn’t be identified with an acceptable degree of certainty")
+                    {
+
+                    }
+
+                    Console.WriteLine(language);
+
+                    StartPoint = val;
+                }
+            }
+        }
+   
+        public static string IdentityLanguage(string sentence)
         {
             var factory = new RankedLanguageIdentifierFactory();
             var identifier = factory.Load(@"C:\Users\Andrew Romanuk\source\Projects\SpilnaSpravaTask2\SpilnaSpravaTask2\LanguageModels\Core14.profile.xml");
 
-            var languages = identifier.Identify("Larin es el mejor");
+            var languages = identifier.Identify(sentence);
 
             var mostCertainLanguage = languages.FirstOrDefault();
             if (mostCertainLanguage != null)
             {
-                string LarMan = mostCertainLanguage.Item1.Iso639_3;
+                string resultLanguage = mostCertainLanguage.Item1.Iso639_3;
 
-                Console.WriteLine(LarMan);
+                return resultLanguage;
             }
-            else
-            {
-                Console.WriteLine("The language couldn’t be identified with an acceptable degree of certainty");
-
-            }
+           
+            return "System error: The language couldn’t be identified with an acceptable degree of certainty";
         }
     }
 }
